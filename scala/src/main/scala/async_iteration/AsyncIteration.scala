aaff39f0aa3b799cff747e0e6ce9b69eda49155e
Danny McClanahan <danieldmcclanahan@gmail.com>
2016-09-20 03:08:06 -0500

    add some functional stuff

--- scala/src/main/scala/async_iteration/AsyncIteration.scala
+++ scala/src/main/scala/async_iteration/AsyncIteration.scala
@@ -10,10 +10,35 @@ trait AsyncIterable[+T] {
 }
 
 trait AsyncIterator[+T] {
-  def next: Future[Option[NextIterator[T]]]
+  def next: Future[Option[Next[T]]]
+  def map[S](f: T => S): AsyncIterator[S] = new MapIterator(this)(f)
+  def filter(f: T => Boolean): AsyncIterator[T] = new FilterIterator(this)(f)
+  def reduce[S](init: S)(f: (S, T) => S): Future[S]
 }
 
-case class NextIterator[+T](
+// functional operations on async iterators
+class MapIterator[S, +T](init: AsyncIterator[S])(
+  f: S => T
+) extends AsyncIterator[T] {
+  override def next: Future[Option[Next[T]]] = init.next.map(_.map {
+    case Next(cur, nxt) => Next(f(cur), new MapIterator(nxt)(f))
+  })
+}
+
+class FilterIterator[+T](init: AsyncIterator[T])(
+  f: T => Boolean
+) extends AsyncIterator[T] {
+  override def next: Future[Option[Next[T]]] = init.next.flatMap(_.map {
+    case Next(cur, nxt) =>
+      if (f(cur)) {
+        Future(Some(Next(cur, new FilterIterator(nxt, f))))
+      } else {
+        new FilterIterator(nxt, f).next
+      }
+  })
+}
+
+case class Next[+T](
   cur: T,
   next: AsyncIterator[T]
 )
@@ -29,16 +54,17 @@ object Paging {
     Some(initOffset), pager, Seq()
   )
 
+  // TODO: optimize map by just applying to curColl!
   class IteratorWithPage[+O, +T, +C <: Traversable[T]](
     offset: Option[O],
     pager: O => Future[Result[O, T, C]],
     curColl: C
   ) extends AsyncIterator[T] {
-    override def next: Future[Option[NextIterator[T]]] =
+    override def next: Future[Option[Next[T]]] =
       curColl.splitHeadTail match {
         case Some((v, rest)) =>
           val nextIt = new IteratorWithPage(offset, pager, rest)
-          Future(Some(NextIterator(v, nextIt)))
+          Future(Some(Next(v, nextIt)))
         case None => offset.map { pager(_).flatMap {
           case Result(off, coll) =>
             new IteratorWithPage(off, pager, coll).next
