a6ad7c670bc4ba1ef2a7bc4c77647cc8c52a358f
Danny McClanahan <danieldmcclanahan@gmail.com>
2016-09-20 14:14:16 -0500

    add asynctraversableonce trait

--- scala/src/main/scala/async_iteration/AsyncIteration.scala
+++ scala/src/main/scala/async_iteration/AsyncIteration.scala
@@ -5,15 +5,54 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 import Util._
 
-trait AsyncIterable[+T] {
+trait AsyncIterable[+T, +F] extends AsyncTraversableOnce[T, F] {
   def iterator: AsyncIterator[T]
 }
 
-trait AsyncIterator[+T] {
-  def next: Future[Option[Next[T]]]
-  def map[S](f: T => S): AsyncIterator[S] = new MapIterator(this)(f)
-  def filter(f: T => Boolean): AsyncIterator[T] = new FilterIterator(this)(f)
-  def reduce[S](init: S)(f: (S, T) => S): Future[S]
+trait AsyncIterator[+T, +F] extends AsyncTraversableOnce[T, F] {
+  def next: F[Option[Next[T]]]
+  // def map[S](f: T => S): AsyncIterator[S] = new MapIterator(this)(f)
+  // def filter(f: T => Boolean): AsyncIterator[T] = new FilterIterator(this)(f)
+  // def reduce[S](init: S)(f: (S, T) => S): Future[S]
+}
+
+case class Next[+T](
+  cur: T,
+  next: AsyncIterator[T]
+)
+
+trait AsyncTraversableOnce[+T, +F] {
+  type Pred = T => Boolean
+  type ATO = AsyncTraversableOnce[T, F]
+
+  def take(n: Int): ATO
+  def takeWhile(f: Pred): ATO
+  def drop(n: Int): ATO
+  def dropWhile(f: Pred): ATO
+  def slice(from: Int, to: Int): ATO
+  def find(f: Pred): F[Option[T]]
+  def forall(f: Pred): F[Boolean]
+  def exists(f: Pred): F[Boolean]
+  def count(f: Pred): F[Int]
+  def foreach(f: T => Unit): F[Unit]
+  def map[S](f: T => S): AsyncTraversableOnce[S, F]
+  def filter(f: Pred): ATO
+  def ++(other: ATO): ATO
+  def isEmpty: F[Boolean]
+  def nonEmpty: F[Boolean]
+  // TODO: add min/max/sum!
+  def maxBy[S : Ordering](f: T => S): F[T]
+  def minBy[S : Ordering](f: T => S): F[T]
+  def zipWith[S, U](other: AsyncTraversableOnce[S, F])(
+    f: (T, S) => U
+  ): AsyncTraversableOnce[U, F]
+
+  // TODO: add other folds!
+  def foldLeft[S](init: S)(f: (S, T) => S): F[S]
+
+  // utilities
+  def enumerate: F[TraversableOnce[T]]
+  def convertFuture[O](implicit conv: F => O): AsyncTraversableOnce[T, O]
 }
 
 // functional operations on async iterators
@@ -37,11 +76,6 @@ class FilterIterator[+T](init: AsyncIterator[T])(
       }
   })
 }
-
-case class Next[+T](
-  cur: T,
-  next: AsyncIterator[T]
-)
 
 object Paging {
   case class Result[+O, +T, +C <: Traversable[T]](
